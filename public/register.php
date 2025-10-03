d0cf0b8348d4710804e2c6c105395c96a0606700
HelenaBohn <helena_b_martins@estudante.sesisenai.org.br>
2025-10-03 11:38:36 -0300

    .

--- public/register.php
+++ public/register.php
@@ -18,8 +18,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro</title>
     <link rel="stylesheet" href="../assets/style.css">
+     <title>Registro</title>
 </head>
 <body>
     
--- src/User.php
+++ src/User.php
@@ -1,20 +1,22 @@
 <?php
-
-class User{
-    private $conn;
+    include '../includes/db_connect.php';
+    class User{
+        private $conn;
 
     public function_construct($db){
-
     $this -> conn = $db;
 }
-    public function register($username, $email, $password){
-    $hash = password_hash($password, PASSWORD_DEFAULT);
-    $sql = "INSERT INTO usuarios (nome, email, senha) VALUES (:username, :email, :password)";
-    $stmt = $this ->bindParam(':username', $username);
-    $stmt = $this ->bindParam(':email', $email);
-    $stmt = $this ->bindParam(':password', $hash);
-    return $stmt -> execute();
 
+    public function register($username, $email, $password){
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO usuarios (nome, email, senha) VALUES (:username, :email, :password)";
+
+        $stmt = $this -> conn->prepare($sql);
+
+        $stmt = $this ->bindParam(':username', $username);
+        $stmt = $this ->bindParam(':email', $email);
+        $stmt = $this ->bindParam(':password', $hash);
+        return $stmt -> execute();
     }
 
     public function login($email, $password){
@@ -30,6 +32,12 @@ class User{
             return false;
         }
 
-   };
-   
+       public function getUserById($userId) {
+            $sql = "SELECT * FROM usuario WHERE id = :id";
+            $stmt = $this -> conn->prepare($sql);
+            $stmt -> bindParam(':id', $userId);
+            $stmt -> execute();
+            return $stmt -> fetch(PDO::FETCH_ASSOC);
+        }
+    };
 ?>
